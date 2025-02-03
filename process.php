fde97cfd9b7c25d79e73f5ce999e98f78e26272f
CubeBuilder <CubeBuilder@web.de>
2025-02-03 07:50:24 +0100

    Delete process.php

--- process.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_POST['textInput'])) {
-    $inputText = htmlspecialchars($_POST['textInput']);  // Sicherheitsvorkehrung gegen XSS-Angriffe
-    header("Location: index.php?text=" . urlencode($inputText));  // Weiterleitung zur Startseite mit dem eingegebenen Text
-    exit;
-}
-?>
